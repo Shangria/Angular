ae6daabac2364ac37d10a884822ca87eda437d59
Shangri <shangrialena@gmail.com>
2022-02-01 17:47:54 +0200

    added httpClient

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {UsersService} from "./users.service";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ang-pj';
+
+
+
+  constructor(private user: UsersService) {
+
+    this.user.getData().subscribe(data => {
+      console.log(data)
+    })
+  }
 }
